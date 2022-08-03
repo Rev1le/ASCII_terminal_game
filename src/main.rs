0c70db1ad23bacde455f5013e640c6588e063cce
Rev1le <rev1le@vivaldi.net>
2022-08-03 11:54:22 +0500

    Delete cycles

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,31 @@ use terminal::Value::TerminalSize;
 pub struct Player {
     hp: i16,
     mana: u8,
+    exp : Experience,
     coords: (i32, i32),
     inventary: Vec<InvItem>,
     emoji_glyph: String
 }
 
+pub struct Experience {
+    lvl: u32,
+    exp: u32
+}
+
 pub struct InvItem{}
 
 pub struct MapGame { rows : Vec<Vec<String>> }
+
+pub struct NonPlayerCharacter {
+    name: String,
+    role: String,
+    quests: Vec<Quest>,
+    coords : (i32, i32),
+    npc_is_alive: bool,
+    emoji_glyph: String
+}
+
+pub struct Quest {}
 
 #[derive(Clone)]
 #[derive(Debug)]
@@ -38,9 +55,9 @@ impl Monster {
         self.hp -= damage;
         if self.hp <= 0 {
             self.mob_is_alive = false;
-            return true;
+            return false
         }
-        false
+        return true
     }
 }
 
@@ -49,9 +66,9 @@ fn main() {
 
     env::set_var("RUST_BACKTRACE", "1");
 
-    let (mut player, mut map, mut mob_vec) = generate_static_object();
+    let (mut player, mut map, mut mob_vec, mut npc_vec) = generate_static_object();
 
-    render_game(&player, &map, &mob_vec); // начальное расположение карты
+    render_game(&player, &map, &mob_vec, &npc_vec); // начальное расположение карты
 
     loop {
         //Получение символов из строки ввода направления движения
@@ -62,18 +79,18 @@ fn main() {
 
         // Добавитть loop, для анализа каддого символа .next()
         match split.next() {
-            Some(x) => move_gg(x, &mut player, &mut mob_vec), //Движение персонаджа по карте и регистрация событий
+            Some(x) => move_gg(x, &mut player, &mut mob_vec, &mut npc_vec), //Движение персонаджа по карте и регистрация событий
             None => println!("Введите команду!")
         }
         if mob_vec.len() == 0 {
             println!("Вы выирали!!!");
             std::process::exit(1);
         }
-        render_game(&player, &map, &mob_vec);
+        render_game(&player, &map, &mob_vec, &npc_vec);
     }
 }
 
-fn generate_static_object() -> (Player, MapGame, Vec<Monster>){
+fn generate_static_object() -> (Player, MapGame, Vec<Monster>, Vec<NonPlayerCharacter>){
     let mut rng = rand::thread_rng();
     //let terminal: Terminal<Stdout> = terminal::stdout();
 
@@ -81,6 +98,7 @@ fn generate_static_object() -> (Player, MapGame, Vec<Monster>){
     let mut player = Player{
         hp: 100,
         mana: 100,
+        exp: Experience{lvl: 1, exp: 0},
         coords: (
             rng.gen_range(0..10), //  x
             rng.gen_range(0..10)),//  y
@@ -122,19 +140,39 @@ fn generate_static_object() -> (Player, MapGame, Vec<Monster>){
         mob_vec.push(monster);
     }
 
+    //Generate NPC in Vector
+    let mut npc_vec: Vec<NonPlayerCharacter> = Vec::new();
+    let one_npc = NonPlayerCharacter{
+    name: String::from("Robert"),
+    role: String::from("citizen"),
+    quests: Vec::new(),
+    coords: (
+            rng.gen_range(0..10),
+            rng.gen_range(0..10)
+            ),
+    npc_is_alive: true,
+    emoji_glyph: String::from(emoji::people_and_body::person_symbol::BUST_IN_SILHOUETTE.glyph)};
+
+    npc_vec.push(one_npc);
+
+
     //return all value
-    return (player, map, mob_vec)
+    return (player, map, mob_vec, npc_vec)
 }
 
 
-fn move_gg(vecmov :(usize, char), player: &mut Player, mob_vec: &mut Vec<Monster>){
+fn move_gg( vecmov :(usize, char),
+            player: &mut Player,
+            mob_vec: &mut Vec<Monster>,
+            npc_vec: &mut Vec<NonPlayerCharacter>)
+            {
 
     let mut rus = None;
     match vecmov.1 {
-        'w' => rus = check_event(player, ('y', -1), &mob_vec),
-        's' => rus = check_event(player, ('y', 1), &mob_vec),
-        'a' => rus = check_event(player, ('x', -1), &mob_vec),
-        'd' => rus = check_event(player, ('x', 1), &mob_vec),
+        'w' => rus = check_event(player, ('y', -1), &mob_vec, &npc_vec),
+        's' => rus = check_event(player, ('y', 1), &mob_vec, &npc_vec),
+        'a' => rus = check_event(player, ('x', -1), &mob_vec, &npc_vec),
+        'd' => rus = check_event(player, ('x', 1), &mob_vec, &npc_vec),
         _ => println!("Неверное напрвление движения. Введите корректные: (w - вверх, s - вниз, a - влево, d - вправо)")
     }
     match rus {
@@ -144,47 +182,20 @@ fn move_gg(vecmov :(usize, char), player: &mut Player, mob_vec: &mut Vec<Monster
                 //                  При обнаружении события вызывался бы метод в структуре Монстр, который отнимал хп
                 //                  как у монстра, так и у игрока).
             'f' => {
-            /////////////////////////////////////////////////////////
-            /*
-                let iter_mob = mob_vec.clone();
-                for mob in &iter_mob{
+
+                let mob_vec_clone = mob_vec.clone();
+
+                for (num, mob) in mob_vec_clone.iter().enumerate() {
                     if mob.id == event.1 {
-                        print!("{:?} {:?}",iter_mob, mob_vec);
-                        let resu = mob_vec[mob.id as usize].loss_of_hp(20);
+                        mob_vec[num].loss_of_hp(20);
                         println!(" Хп монтстра {}", mob.hp);
-                        if resu {
-                            mob_vec.remove(mob.id as usize);
+
+                        if mob_vec[num].mob_is_alive == false {
+                            mob_vec.remove(num);
+                            break;
                         }
                     }
                 }
-                */
-            /////////////////////////////////////////////////////////////
-            //let iter_mob = mob_vec.clone();
-                for mut mob in mob_vec.into_iter(){
-                    if mob.id == event.1 {
-                        mob.loss_of_hp(20);
-                        println!(" Хп монтстра {}", mob.hp);
-
-                        // mob.hp <= 0 {
-                        //mob_vec.remove(mob.id as usize);
-                    }
-                }
-
-                for mob_num in 0..mob_vec.len() {
-                    if mob_vec[mob_num].mob_is_alive == false {
-                        mob_vec.remove(mob_num);
-                        break;
-                        //println!("{:?}", mob_vec[mob_num]);
-                    }
-                }
-/*
-                let iter_mob = mob_vec.clone();
-                for mob in iter_mob {
-                    if mob.hp <= 0 {
-                        mob_vec.remove(mob.id as usize);
-                    }
-                }
-                */
             },
             _ => {}
             }
@@ -195,7 +206,7 @@ fn move_gg(vecmov :(usize, char), player: &mut Player, mob_vec: &mut Vec<Monster
     println!("действие {:?}", rus);
 }
 
-fn render_game(player: &Player, map :&MapGame, mob_vec : &Vec<Monster>){
+fn render_game(player: &Player, map: &MapGame, mob_vec: &Vec<Monster>, npc_vec: &Vec<NonPlayerCharacter>){
    //terminal.act(Action::ClearTerminal(Clear::All)).unwrap();
     {
         let mut x = 0;
@@ -212,10 +223,17 @@ fn render_game(player: &Player, map :&MapGame, mob_vec : &Vec<Monster>){
                     free_cell = false;
                 }
 
-                //replaced Mobs
+                // replaced Mobs
                 for mob in mob_vec.iter() {
                     if mob.coords == (x, y) && free_cell {
                         print!("{}", mob.mob_glyph);
+                        free_cell = false;
+                    }
+                }
+
+                for npc in npc_vec.iter() {
+                    if npc.coords == (x, y) && free_cell {
+                        print!("{}", npc.emoji_glyph);
                         free_cell = false;
                     }
                 }
@@ -242,7 +260,10 @@ fn render_game(player: &Player, map :&MapGame, mob_vec : &Vec<Monster>){
 
 
 fn check_event(player: &mut Player,
-               move_coord :(char, i32), mobs_vec : &Vec<Monster>) -> Option<(char, i32)>{
+               move_coord :(char, i32),
+               mobs_vec : &Vec<Monster>,
+               npc_vec: &Vec<NonPlayerCharacter>)
+               -> Option<(char, i32)>{
 
     let mut has_event = false;
 
@@ -255,8 +276,18 @@ fn check_event(player: &mut Player,
                     return Some(('f', mob.id)) //возвращает символ 'f' - fight
                 }
             }
+
+            for mut npc in npc_vec{
+                if (player.coords.1 + move_coord.1 == npc.coords.1)
+                    && (player.coords.0 == npc.coords.0) {
+                    println!("Диалог!!!");
+                    return Some(('d', 2)) //возвращает символ 'f' - fight
+                }
+            }
+
             player.coords.1 += move_coord.1;
         },
+
         'x' => {
             for mob in mobs_vec{
                 if (player.coords.0 + move_coord.1 == mob.coords.0)
@@ -265,6 +296,15 @@ fn check_event(player: &mut Player,
                     return Some(('f', mob.id)) //возвращает символ 'f' - fight
                 }
             }
+
+            for npc in npc_vec{
+                if (player.coords.0 + move_coord.1 == npc.coords.0)
+                    && (player.coords.1 == npc.coords.1) {
+                    println!("Диалог!!!");
+                    return Some(('d', 2)) //возвращает символ 'f' - fight
+                }
+            }
+
             player.coords.0 += move_coord.1;
         },
         _ => {}
